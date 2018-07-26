30c06c2b9b4a108e1b5e7b87cda04b156a07f934
memolop <inglopezvargas@gmail.com>
2018-07-26 15:19:46 -0500

    rotaciones

---
+++ tarea1rotacion.js
@@ -0,0 +1,45 @@
+let mipalabra = 'hola'
+let micadena = [ 'raul', 'vino', 'rosa',  'olah', 'laho', 'ahol']
+
+
+
+function rotaciones(lapalabra) {
+
+    let rotacion = []
+    for (let i = 0; i < lapalabra.length; i++) {
+
+
+        let parte1 = lapalabra.substring(i)
+        let parte2 = lapalabra.substring(0, i)
+
+
+
+
+        rotacion.push(parte1 + parte2)
+
+    }
+
+
+    return rotacion
+
+
+}
+
+function contain_all_rots(palabra, cadena) {
+    let mirotacion = rotaciones(palabra)
+    console.log(mirotacion)
+    for (let j = 0; j < mirotacion.length; j++) {
+        // console.log(!cadena.includes(mirotacion[j]))
+        if (!cadena.includes(mirotacion[j])) {
+            console.log(mirotacion[j])
+            return false
+        }
+
+    }
+    return true
+
+
+
+}
+
+console.log(contain_all_rots(mipalabra, micadena))
